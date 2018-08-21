6ba981128fd1df28f75e9b05ad15e6551f969824
Aman Dua -X (amdua - ARICENT TECHNOLOGIES MAURITIUS LIMITED at Cisco) <amdua@partnet.cisco.com>
2018-08-21 14:12:12 +0530

    some changes

--- noProxy.bat
+++ noProxy.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections" /v DefaultConnectionSettings /t REG_DWORD /d 1101 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections" /v DefaultConnectionSettings /t REG_DWORD /d 0011 /f
